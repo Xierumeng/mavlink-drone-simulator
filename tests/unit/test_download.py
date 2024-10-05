e54dd3cb1ccc8980d114e47d584a5b6509c86ad4
Xierumeng <22666760+Xierumeng@users.noreply.github.com>
2024-10-05 16:23:35 -0400

    Download and save

---
+++ tests/unit/test_download.py
@@ -0,0 +1,50 @@
+"""
+Test download.
+"""
+
+import pathlib
+
+import pytest
+
+from modules.download import download
+
+
+# Test functions use test fixture signature names and access class privates
+# No enable
+# pylint: disable=protected-access,redefined-outer-name
+
+
+@pytest.fixture
+def names() -> list[download.DownloadNameMap]:  # type: ignore
+    """
+    Single name.
+    """
+    result, name = download.DownloadNameMap.create(
+        "ArduCopter.elf",
+        "ArduCopter.exe",
+    )
+    assert result
+    assert name is not None
+
+    yield [name]  # type: ignore
+
+
+def test_single_file(names: list[download.DownloadNameMap], tmp_path: pathlib.Path) -> None:
+    """
+    Basic test to download and save a single file.
+
+    This test is fragile as it depends on a remote server!
+    """
+    # Setup
+    base_url = "https://firmware.ardupilot.org/Tools/MissionPlanner/sitl/CopterStable/"
+
+    # Build a temporary directory using tmp_path so
+    # the files are cleaned after the tests are run
+    tmp_path.mkdir(parents=True, exist_ok=True)
+
+    # Run
+    result = download.download_and_save(base_url, names, tmp_path)
+
+    # Check
+    assert result
+    assert pathlib.Path(tmp_path, names[0].local_name).stat().st_size == 9511971
