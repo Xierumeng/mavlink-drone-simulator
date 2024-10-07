a4648bf6f05b058005795ac4059e130b0625bdc2
Xierumeng <22666760+Xierumeng@users.noreply.github.com>
2024-10-07 00:26:44 -0400

    Delete directory

--- modules/download/download.py
+++ modules/download/download.py
@@ -3,9 +3,37 @@ Download and save files from the World Wide Web.
 """
 
 import pathlib
+import shutil
 import urllib.request
 
 from . import download_name_map
+
+
+def delete_directory(directory_path: pathlib.Path) -> bool:
+    """
+    Delete the provided directory.
+
+    directory_path: The directory.
+
+    Return: Success.
+    """
+    if not directory_path.exists():
+        print("ERROR: Path doesn't exist")
+        return False
+
+    if not directory_path.is_dir():
+        print("ERROR: Not a directory")
+        return False
+
+    try:
+        shutil.rmtree(directory_path)
+        return True
+    # Catching all exceptions for library call
+    # pylint: disable-next=broad-exception-caught
+    except Exception as exception:
+        print(f"ERROR: Could not delete directory: {exception}")
+
+    return False
 
 
 def download_and_save(
--- tests/unit/test_download.py
+++ tests/unit/test_download.py
@@ -16,6 +16,16 @@ from modules.download import download_name_map
 
 
 @pytest.fixture
+def test_directory_create(tmp_path: pathlib.Path) -> pathlib.Path:  # type: ignore
+    """
+    Create temporary directory.
+    """
+    tmp_path.mkdir(parents=True, exist_ok=True)
+
+    yield tmp_path
+
+
+@pytest.fixture
 def names() -> list[download_name_map.DownloadNameMap]:  # type: ignore
     """
     Single name.
@@ -30,24 +40,85 @@ def names() -> list[download_name_map.DownloadNameMap]:  # type: ignore
     yield [name]  # type: ignore
 
 
-def test_single_file(
-    names: list[download_name_map.DownloadNameMap], tmp_path: pathlib.Path
-) -> None:
+class TestDeleteDirectory:
     """
-    Basic test to download and save a single file.
-
-    This test is fragile as it depends on a remote server!
+    Test delete_directory() function.
     """
-    # Setup
-    base_url = "https://firmware.ardupilot.org/Tools/MissionPlanner/sitl/CopterStable/"
 
-    # Build a temporary directory using tmp_path so
-    # the files are cleaned after the tests are run
-    tmp_path.mkdir(parents=True, exist_ok=True)
+    def test_normal(self, test_directory_create: pathlib.Path) -> None:
+        """
+        Basic test.
+        """
+        # Setup
+        test_file_path = pathlib.Path(test_directory_create, "test_file.txt")
+        with open(test_file_path, "w", encoding="utf-8") as test_file:
+            test_file.write("Test")
 
-    # Run
-    result = download.download_and_save(base_url, names, tmp_path)
+        assert test_file_path.exists()
 
-    # Check
-    assert result
-    assert pathlib.Path(tmp_path, names[0].local_name).stat().st_size == 9511971
+        # Run
+        result = download.delete_directory(test_directory_create)
+
+        # Check
+        assert result
+        assert not test_directory_create.exists()
+
+    def test_nonexistent_path(self, test_directory_create: pathlib.Path) -> None:
+        """
+        Path doesn't exist.
+        """
+        # Setup
+        nonexistent_directory = pathlib.Path(test_directory_create, "nonexistent_directory")
+
+        # Run
+        result = download.delete_directory(nonexistent_directory)
+
+        # Check
+        assert not result
+        assert test_directory_create.exists()
+
+    def test_not_a_directory(self, test_directory_create: pathlib.Path) -> None:
+        """
+        Non directory path.
+        """
+        # Setup
+        test_file_path = pathlib.Path(test_directory_create, "test_file.txt")
+        with open(test_file_path, "w", encoding="utf-8") as test_file:
+            test_file.write("Test")
+
+        assert test_file_path.exists()
+
+        # Run
+        result = download.delete_directory(test_file_path)
+
+        # Check
+        assert not result
+        assert test_directory_create.exists()
+
+
+class TestDownloadAndSave:
+    """
+    Test download_and_save() function.
+    """
+
+    def test_single_file(
+        self, names: list[download_name_map.DownloadNameMap], tmp_path: pathlib.Path
+    ) -> None:
+        """
+        Basic test to download and save a single file.
+
+        This test is fragile as it depends on a remote server!
+        """
+        # Setup
+        base_url = "https://firmware.ardupilot.org/Tools/MissionPlanner/sitl/CopterStable/"
+
+        # Build a temporary directory using tmp_path so
+        # the files are cleaned after the tests are run
+        tmp_path.mkdir(parents=True, exist_ok=True)
+
+        # Run
+        result = download.download_and_save(base_url, names, tmp_path)
+
+        # Check
+        assert result
+        assert pathlib.Path(tmp_path, names[0].local_name).stat().st_size == 9511971
